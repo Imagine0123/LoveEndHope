1df5fe6f475be7fc41f75a416f5c36ddee2ad3b5
Imagine0123 <127961339+Imagine0123@users.noreply.github.com>
2025-06-18 18:20:58 +0700

    Added Volume control
    Now the sliders in the options actually work

--- Assets/Script/Menu/MainMenu.cs
+++ Assets/Script/Menu/MainMenu.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
     public void Start()
     {
+        LoadVolume();
         MusicManager.instance.PlayMusic("MenuMusic");
     }
     public void Play()
@@ -17,5 +23,29 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
